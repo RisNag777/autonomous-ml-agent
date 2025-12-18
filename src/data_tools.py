084ba82d9d51c2fb71c6f14c7abfe1e76fc6f751
RisNag777 <risnag1995@gmail.com>
2025-12-18 16:52:48 -0500

    [FIX] inspect_dataset function - Sum df

--- src/data_tools.py
+++ src/data_tools.py
@@ -6,7 +6,7 @@ def inspect_dataset(file_path):
     profile = {
             "n_rows": df.shape[0],
             "n_columns": df.shape[1],
-            "missing_values": df.isna().sum.to_dict(),
+            "missing_values": df.isna().sum().to_dict(),
             "column_types": df.dtypes.apply(lambda x: str(x)).to_dict()
             }
     return df, profile
